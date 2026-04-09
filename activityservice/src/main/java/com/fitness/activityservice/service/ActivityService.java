40aa2297bcc8c0d142feda552564fa7c040f9c0a
Aditya Raj <its.rajofficial26@gmail.com>
2026-04-09 13:47:21 +0530

    added activityServices and setting up eureka server

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,44 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.AllArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity savedActivity) {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(savedActivity.getId());
+        response.setUserId(savedActivity.getUserId());
+        response.setType(savedActivity.getType());
+        response.setDuration(savedActivity.getDuration());
+        response.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        response.setStartTime(savedActivity.getStartTime());
+        response.setAdditionalMetrics(savedActivity.getAdditionalMetrics());
+        response.setCreatedAt(savedActivity.getCreatedAt());
+        response.setUpdatedAt(savedActivity.getUpdatedAt());
+        return response;
+    }
+}
